5542bd1d39814f78765f4121dd1379d3b163ee9b
George Huan <george.huan1994@foxmail.com>
2023-03-25 02:10:40 +0800

    Refactor some properties, and replace the GridList with 2D array

--- TicTacToe/Assets/Scripts/Definiton/GameConstant.cs
+++ TicTacToe/Assets/Scripts/Definiton/GameConstant.cs
@@ -7,7 +7,7 @@ public static class GameConstant
     public static readonly Color PlayerThemeColor = new Color(0.78f, 1, 0.42f, 1);
     public static readonly Color ComputeThemeColor = new Color(1, 0.23f, 0.23f, 1);
     public static readonly Color GridColor = new Color(0.2f, 0, 0.5f, 1);
-    public static readonly Color GridDrawColor = new Color(0.93f, 0.73f, 0, 1);
+    public static readonly Color GridTiesColor = new Color(0.93f, 0.73f, 0, 1);
     public static readonly Color GridHighlightColor = new Color(0.4f, 0.8f, 0.495f, 1);
 
     public const float GridInitShowInterval = 0.03f;
